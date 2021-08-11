71a714fba130d321e3dea6ed423ff147fc0dea36
Michael Vassilyev <michaelvassilyevmvc@gmail.com>
2021-08-12 00:43:42 +0600

    Create Client

---
+++ Client/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Client";
+            Uri address = new Uri("http://localhost:4050/IContract");
+            BasicHttpBinding binding = new BasicHttpBinding();
+            EndpointAddress endpoint = new EndpointAddress(address);
+            ChannelFactory<IContract> channelFactory = new ChannelFactory<IContract>(binding, endpoint);
+            IContract channel = channelFactory.CreateChannel();
+            channel.Say("It a live!!!!");
+            Console.ReadKey();
+
+
+        }
+    }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.ServiceModel;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Server
 {
@@ -12,12 +8,12 @@ namespace Server
         static void Main(string[] args)
         {
             Console.Title = "SERVER";
-            Uri uri = new Uri("http://localhost:4050/IContract");
+            Uri address = new Uri("http://localhost:4050/IContract");
             BasicHttpBinding binding = new BasicHttpBinding();
             Type contract = typeof(IContract);
 
-            ServiceHost host = new ServiceHost(contract, uri);
-            host.AddServiceEndpoint(contract, binding, uri);
+            ServiceHost host = new ServiceHost(typeof(Service));
+            host.AddServiceEndpoint(contract, binding, address);
             host.Open();
 
             Console.WriteLine("Ожидание сообщения от клиента");
