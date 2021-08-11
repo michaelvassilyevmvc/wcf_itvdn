f4aff73a047354f4d9de8b6659a719e1fb6e9ab7
Michael Vassilyev <michaelvassilyevmvc@gmail.com>
2021-08-12 00:02:57 +0600

    First init

---
+++ Server/IContract.cs
@@ -0,0 +1,11 @@
+﻿using System.ServiceModel;
+
+namespace Server
+{
+    [ServiceContract]
+    public interface IContract
+    {
+        [OperationContract]
+        void Say(string input);
+    }
+}
---
+++ Server/Service.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Server
+{
+    public class Service : IContract
+    {
+        public void Say(string input)
+        {
+            Console.WriteLine($"Клинет прислал сообщение {input}");
+        }
+    }
+}
